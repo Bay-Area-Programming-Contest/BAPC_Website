6c3b997a10e9ae7e90541c1c111056e963fb62e2
qpwoeirut <stanleyzhong8@gmail.com>
2024-01-23 20:57:11 -0800

    Adjust homepage positioning, use simpler CSS animation to replace home.js animation

--- scripts/home.js
+++ scripts/home.js
@@ -1,37 +1,11 @@
 function scrollToSection() {
-    const secondSection = document.getElementsByClassName('WhatIsBAPC')[0];
+    const secondSection = document.querySelector('.WhatIsBAPC');
     secondSection.scrollIntoView({behavior: 'smooth'});
 }
 
 window.addEventListener("load", () => {
-    // Delay the appearance of the button by 2000 milliseconds (2 seconds)
-    setTimeout(function () {
-        const delayedButton = document.getElementById('delayed-button');
-        delayedButton.style.display = 'block'; // Show the button
-        setTimeout(function () {
-            delayedButton.style.opacity = '1'; // Make the button visible
-        }, 50);
-    }, 2000);
-
-
-    // make the image bounce up and down smoothly
     const delayedButton = document.getElementById('delayed-button');
-    let bounceUp = true;
-    let position = delayedButton.getBoundingClientRect().top;
-    let curposition = position;
-    setInterval(function () {
-        if (bounceUp) {
-            curposition += 0.01;
-            delayedButton.style.top = curposition + '%';
-            if (curposition >= position+1) {
-                bounceUp = false;
-            }
-        } else {
-            curposition -= 0.01;
-            delayedButton.style.top = curposition + '%';
-            if (curposition <= position) {
-                bounceUp = true;
-            }
-        }
-    }, 7);
+    setTimeout(function () {
+        delayedButton.classList.add("active");
+    }, 2000);
 });
