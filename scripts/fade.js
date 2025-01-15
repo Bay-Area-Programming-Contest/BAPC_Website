dce88e78561bb3d8bfa3325b188a7a9f56e5bf1b
qpwoeirut <stanleyzhong8@gmail.com>
2025-01-14 21:16:11 -0800

    Format, use const, use template strings, remove logs, remove self-evident comments

--- scripts/fade.js
+++ scripts/fade.js
@@ -1,31 +1,24 @@
-window.addEventListener("load", () => {    
-    // Select all the text boxes
+window.addEventListener("load", () => {
     const textBoxes = document.querySelectorAll('.fade-in-text');
 
-    // Calculate the center of the screen
     const screenCenter = window.innerHeight / 2;
     const cutoff = 0.7;
 
-    function fade () {
-    // Iterate over each text box
-    textBoxes.forEach(textBox => {
-        // Calculate the distance of the text box from the center of the screen
-        console.log(textBox.getBoundingClientRect().top, textBox);
-        const distanceFromCenter = Math.abs(screenCenter - (textBox.getBoundingClientRect().top + textBox.getBoundingClientRect().height / 2));
-        if(distanceFromCenter < cutoff * screenCenter) {
-        textBox.style.opacity = 1;
-            return;
-        }
-        const distanceFromCutOff = distanceFromCenter - cutoff * screenCenter;
-        // add some padding to the cutoff
-        // Normalize this distance to a value between 0 and 1
-        const opacity = (1 - distanceFromCutOff / ((1 - cutoff) * screenCenter));
+    function fade() {
+        textBoxes.forEach(textBox => {
+            const distanceFromCenter = Math.abs(screenCenter - (textBox.getBoundingClientRect().top + textBox.getBoundingClientRect().height / 2));
+            if (distanceFromCenter < cutoff * screenCenter) {
+                textBox.style.opacity = "1";
+            } else {
+                // Add some padding to the cutoff
+                const distanceFromCutOff = distanceFromCenter - cutoff * screenCenter;
 
-
-        // Set the opacity of the text box
-        textBox.style.opacity = opacity;
-    });
+                // Normalize this distance to a value between 0 and 1
+                textBox.style.opacity = (1 - distanceFromCutOff / ((1 - cutoff) * screenCenter)).toString();
+            }
+        });
     }
+
     fade();
     window.addEventListener('scroll', fade);
 });
--- scripts/glasshover.js
+++ scripts/glasshover.js
@@ -1,13 +1,12 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
+    document.getElementById("glass-card-container").onmousemove = e => {
+        for (const card of document.getElementsByClassName("glass-card")) {
+            const rect = card.getBoundingClientRect(),
+                x = e.clientX - rect.left,
+                y = e.clientY - rect.top;
 
-document.getElementById("glass-card-container").onmousemove = e => {
-    for(const card of document.getElementsByClassName("glass-card")) {
-      const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-  
-      card.style.setProperty("--mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
-    };
-  }
+            card.style.setProperty("--mouse-x", `${x}px`);
+            card.style.setProperty("--mouse-y", `${y}px`);
+        }
+    }
 });
