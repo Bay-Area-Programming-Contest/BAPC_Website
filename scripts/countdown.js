3cf95d42314a7f56801d895a6a6f4cf3cfb9eea4
qpwoeirut <stanleyzhong8@gmail.com>
2025-01-14 21:01:40 -0800

    Format file, use let/const, set textContent instead of innerHTML

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,40 +1,38 @@
 // Set the date we're counting down to
-var countDownDate = new Date("March 3, 2024 11:00:00").getTime();
-           
+const countDownDate = new Date("March 3, 2024 11:00:00").getTime();
+
 // Update the count down every 1 second
-var x = setInterval(function() {
+const x = setInterval(function () {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+    // Get today's date and time
+    const now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+    // Find the distance between now and the count down date
+    const distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  hours = hours + days*24;
-  if(seconds < 10){
-    seconds = "0" + seconds;
-  }
-    if(minutes < 10){
+    hours = hours + days * 24;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    if (minutes < 10) {
         minutes = "0" + minutes;
     }
-    if(hours < 10){
+    if (hours < 10) {
         hours = "0" + hours;
     }
 
 
-  // Display the result in the element with id="demo"
-  document.getElementById("date").innerHTML = hours + " : "
-  + minutes + " : " + seconds;
+    document.getElementById("date").textContent = `${hours} : ${minutes} : ${seconds}`;
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("date").innerHTML = "CONTEST IS TODAY!";
-  }
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("date").textContent = "CONTEST IS TODAY!";
+    }
 }, 1000);
