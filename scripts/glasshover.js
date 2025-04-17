49738c042c365d5b58885bfbe31650b03558fda6
qpwoeirut <stanleyzhong8@gmail.com>
2025-04-17 01:16:41 -0700

    Separate out the mousemove handler and the requestAnimationFrame calls

--- scripts/glasshover.js
+++ scripts/glasshover.js
@@ -1,18 +1,19 @@
-window.addEventListener("load", function () {
-    let animationFrameId = null;
+window.addEventListener("load", () => {
+    let clientX = 0, clientY = 0;
 
     document.addEventListener("mousemove", (e) => {
-        if (!animationFrameId) {
-            animationFrameId = requestAnimationFrame(() => {
-                for (const card of document.getElementsByClassName("glass-card")) {
-                    const rect = card.getBoundingClientRect();
-                    const x = e.clientX - rect.left;
-                    const y = e.clientY - rect.top;
-                    card.style.setProperty("--mouse-x", `${x}px`);
-                    card.style.setProperty("--mouse-y", `${y}px`);
-                }
-                animationFrameId = null;
-            });
-        }
+        clientX = e.clientX;
+        clientY = e.clientY;
     });
+
+    const updateCards = () => {
+        for (const card of document.getElementsByClassName("glass-card")) {
+            const rect = card.getBoundingClientRect();
+            card.style.setProperty("--mouse-x", `${clientX - rect.left}px`);
+            card.style.setProperty("--mouse-y", `${clientY - rect.top}px`);
+        }
+        requestAnimationFrame(updateCards);
+    };
+
+    updateCards();
 });
